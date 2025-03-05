6cb57a09f46ae91cb90fc89dc557cfb31f8e1bbe
Gabriel Forchesatto <gabrielvalendorf@gmail.com>
2025-03-05 17:47:41 -0300

    feat: Adicao cenario deveraSomarDoisValoresCorretamente e melhorias maximizar tela

--- src/main/java/sikulix/calculadora/pages/CalculadoraPage.java
+++ src/main/java/sikulix/calculadora/pages/CalculadoraPage.java
@@ -1,9 +1,6 @@
 package sikulix.calculadora.pages;
 
-import org.sikuli.script.App;
-import org.sikuli.script.FindFailed;
-import org.sikuli.script.Key;
-import org.sikuli.script.Pattern;
+import org.sikuli.script.*;
 import sikulix.BaseScreen;
 
 public class CalculadoraPage extends BaseScreen {
@@ -17,12 +14,16 @@ public class CalculadoraPage extends BaseScreen {
         return new Pattern(BASE_PATH_CALCULADORA + "mais_button.png");
     }
 
-    public static Pattern igualdadeButton() {
-        return new Pattern(BASE_PATH_CALCULADORA + "igualdade_button.png");
-    }
-
     public static Pattern padraoLabel() {
         return new Pattern(BASE_PATH_CALCULADORA + "padrao_label.png");
+    }
+
+    public static Pattern appMinimizadoLabel() {
+        return new Pattern(BASE_PATH_CALCULADORA + "app_minimizado_label.png");
+    }
+
+    public static Pattern limparValoresButton() {
+        return new Pattern(BASE_PATH_CALCULADORA + "limpar_valores_button.png");
     }
 
     public static void calculadoraLabelWait() throws FindFailed {
@@ -31,6 +32,10 @@ public class CalculadoraPage extends BaseScreen {
 
     public static void maisButtonWait() throws FindFailed {
         screen.wait(maisButton(), TIME_OUT_SCREEN);
+    }
+
+    public static Match appMinimizadoExists() throws FindFailed {
+        return screen.exists(appMinimizadoLabel(), 2);
     }
 
     public static void numerosSendKeys(String valor) throws FindFailed {
